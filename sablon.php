a327cb938235e9c92346bef30f7ef5727b626b0d
raihanhasbii <hasbiraihan53@gmail.com>
2024-07-17 22:03:15 +0700

    first commit

---
+++ sablon.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Kopi Kenangan Senja</title>
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap"
+    rel="stylesheet">
+
+  <!-- Feather Icons -->
+  <script src="https://unpkg.com/feather-icons"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+  <!-- My Style -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <!-- Navbar start -->
+  <nav class="navbar">
+    <a href="#" class="navbar-logo">Teras <span>Sablon</span>.</a>
+
+    <div class="navbar-nav">
+      <a href="#home">Home</a>
+      <a href="#about">Tentang Kami</a>
+      <a href="#products">Produk</a>
+      <a href="#menu">Gallery</a>
+      <a href="#contact">Kontak</a>
+    </div>
+
+    <div class="navbar-extra">
+      <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
+    </div>
+  </nav>
+  <!-- Navbar end -->
+
+  <!-- Hero Section start -->
+  <section class="hero" id="home">
+    <div class="mask-container">
+      <main class="content">
+        <h1>Ekspresikan Karyamu di <span>Teras Sablon</span></h1>
+        <p>Desain Suka Suka Dengan Harga Istimewa Teras Sablon Tempatnya</p>
+      </main>
+    </div>
+  </section>
+  <!-- Hero Section end -->
+
+  <!-- About Section start -->
+  <section id="about" class="about">
+    <h2><span>Tentang</span> Kami</h2>
+
+    <div class="row">
+      <div class="about-img">
+        <img src="img/about.jpg" alt="Tentang Kami">
+      </div>
+      <div class="content">
+        <h3>Kenapa memilih sablon kami?</h3>
+        <p>Sablon adalah teknik pencetakan yang melibatkan penggunaan stensil untuk mengaplikasikan tinta ke berbagai permukaan, seperti kain, kertas, atau plastik. Metode ini terkenal karena kemampuannya menghasilkan gambar yang tajam dan detail pada berbagai media. Sablon sering digunakan untuk membuat kaos, poster, dan berbagai produk kreatif lainnya. </p>
+        <p>Keunggulan sablon terletak pada kemampuannya mencetak dalam jumlah besar dengan konsistensi tinggi, membuatnya menjadi pilihan populer di industri kreatif dan komersial. Dengan berbagai jenis tinta dan metode pencetakan, sablon menawarkan fleksibilitas dan daya tahan yang menjadikannya pilihan utama bagi banyak seniman dan pengusaha.</p>
+      </div>
+    </div>
+  </section>
+  <!-- About Section end -->
+
+  <!-- Products Section start -->
+  <section class="products" id="products">
+    <h2><span>Produk Unggulan</span> Kami</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo unde eum, ab fuga possimus iste.</p>
+
+    <div class="row">
+      <div class="product-card">
+        <div class="product-icons">
+          <a href="#" class="item-detail-button"><i data-feather="eye"></i></a>
+        </div>
+        <div class="product-image">
+          <img src="img/products/1.jpg" alt="Product 1">
+        </div>
+        <div class="product-content">
+          <h3>Bahan 1</h3>
+        </div>
+      </div>
+      <div class="product-card">
+        <div class="product-icons">
+          <a href="#" class="item-detail-button"><i data-feather="eye"></i></a>
+        </div>
+        <div class="product-image">
+          <img src="img/products/1.jpg" alt="Product 1">
+        </div>
+        <div class="product-content">
+          <h3>Bahan 2</h3>
+        </div>
+      </div>
+      <div class="product-card">
+        <div class="product-icons">
+          <a href="#" class="item-detail-button"><i data-feather="eye"></i></a>
+        </div>
+        <div class="product-image">
+          <img src="img/products/1.jpg" alt="Product 1">
+        </div>
+        <div class="product-content">
+          <h3>Bahan 3</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Products Section end -->
+
+    <!-- Menu Section start -->
+    <section id="menu" class="menu">
+    <h2><span>Gallery</span> Kami</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita, repellendus numquam quam tempora voluptatum.
+    </p>
+
+    <div class="row">
+      <div class="menu-card">
+        <img src="img/bahan/1.jpg" alt="Espresso" class="menu-card-img">
+      </div>
+      <div class="menu-card">
+        <img src="img/bahan/1.jpg" alt="Espresso" class="menu-card-img">
+      </div>
+      <div class="menu-card">
+        <img src="img/bahan/1.jpg" alt="Espresso" class="menu-card-img">
+ 
+      </div>
+      <div class="menu-card">
+        <img src="img/bahan/1.jpg" alt="Espresso" class="menu-card-img">
+      </div>
+      <div class="menu-card">
+        <img src="img/bahan/1.jpg" alt="Espresso" class="menu-card-img">
+      </div>
+      <div class="menu-card">
+        <img src="img/bahan/1.jpg" alt="Espresso" class="menu-card-img">
+      </div>
+    </div>
+  </section>
+  <!-- Menu Section end -->
+
+  <!-- Contact Section start -->
+  <section id="contact" class="contact">
+    <h2><span>Kontak</span> Kami</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, provident.
+    </p>
+
+    <div class="row">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1982.4367788335114!2d106.78564141852854!3d-6.410284273734743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69e916574025bb%3A0x6763a603a0405509!2sTERAS%20SABLON!5e0!3m2!1sid!2sid!4v1721220716807!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
+
+      <form action="">
+        <div class="input-group">
+          <i data-feather="user"></i>
+          <input type="text" placeholder="nama">
+        </div>
+        <div class="input-group">
+          <i data-feather="mail"></i>
+          <input type="text" placeholder="email">
+        </div>
+        <div class="input-group">
+          <i data-feather="phone"></i>
+          <input type="text" placeholder="no hp">
+        </div>
+        <button type="submit" class="btn">kirim pesan</button>
+      </form>
+
+    </div>
+  </section>
+  <!-- Contact Section end -->
+
+  <!-- Footer start -->
+  <footer>
+    <div class="socials">
+      <a href="#"><i data-feather="instagram"></i></a>
+      <a href="#"><i data-feather="twitter"></i></a>
+      <a href="#"><i data-feather="facebook"></i></a>
+      <a href="#"><i class="bi bi-whatsapp"></i></a>
+    </div>
+
+    <div class="links">
+      <a href="#home">Home</a>
+      <a href="#about">Tentang Kami</a>
+      <a href="#products">Produk</a>
+      <a href="#menu">Gallery</a>
+      <a href="#contact">Kontak</a>
+    </div>
+
+    <div class="credit">
+      <p>Created by <a href="">teras</a>. | &copy; 2023.</p>
+    </div>
+  </footer>
+  <!-- Footer end -->
+
+  <!-- Modal Box Item Detail start -->
+  <div class="modal" id="item-detail-modal">
+    <div class="modal-container">
+      <a href="#" class="close-icon"><i data-feather="x"></i></a>
+      <div class="modal-content">
+        <img src="img/products/1.jpg" alt="Product 1">
+        <div class="product-content">
+          <h3>Product 1</h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident, tenetur cupiditate facilis obcaecati
+            ullam maiores minima quos perspiciatis similique itaque, esse rerum eius repellendus voluptatibus!</p>
+          <a href="#"><i class="bi bi-whatsapp"></i> <span>whatsapp </span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Modal Box Item Detail end -->
+
+  <!-- Whatsapp Fix Start -->
+  <div class="whatsapp">
+  <a href="#"><i class="bi bi-whatsapp"></i></a>
+  </div>
+
+  <!-- Feather Icons -->
+  <script>
+    feather.replace()
+  </script>
+
+  <!-- My Javascript -->
+  <script src="js/script.js"></script>
+</body>
+
+</html>
